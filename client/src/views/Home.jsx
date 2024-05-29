623e5655df3b56ff01353f2ba9ef52f270cf4864
camilo Navas <navasochoa@hotmail.com>
2024-05-29 11:11:58 -0500

    inicio formulario controlado SignUp

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { VITE_GET_PARTICIPANTS } from "../config/env.config";
 import axios from "axios";
-import Participants from "../components/viewHome/Participants";
+import Participants from "../components/Participants";
+import SignUp from "../components/SignUp";
 
 const Home = () => {
   const [participants, setParticipants] = useState([]);
@@ -20,13 +21,14 @@ const Home = () => {
   return (
     <main>
       {participants.map((participant) => (
-        <Participants 
-        key={participant.id}
-        id={participant.id}
-        nick={participant.nick}
-        discord={participant.discord}
+        <Participants
+          key={participant.id}
+          id={participant.id}
+          nick={participant.nick}
+          discord={participant.discord}
         />
       ))}
+      <SignUp />
     </main>
   );
 };
