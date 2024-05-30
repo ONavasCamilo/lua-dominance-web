3f97f71bf771a6321b5bcbece0fd56eb8f0998a7
camilo Navas <navasochoa@hotmail.com>
2024-05-30 07:33:12 -0500

    profile solo se muestra iniciada la sesion

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,18 +2,16 @@ import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    const participant = useSelector((state) => state.participant)
-    return (
-        <nav>
-            <ul>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/profile">Profile</NavLink>
-                {!participant.nick && (
-                    <NavLink to="/signin">SignIn</NavLink>
-                )}
-            </ul>
-        </nav>
-    )
+  const participant = useSelector((state) => state.participant);
+  return (
+    <nav>
+      <ul>
+        <NavLink to="/">Home</NavLink>
+        {participant.nick && <NavLink to="/profile">Profile</NavLink>}
+        {!participant.nick && <NavLink to="/signin">SignIn</NavLink>}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
